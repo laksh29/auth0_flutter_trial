ca051f7347e6e3574d66fa891548eefd486b3755
Laksh Samdariya <lakshsamdariya29@gmail.com>
2022-12-18 22:58:29 +0530

    auth0 integration

---
+++ android/app/src/main/kotlin/com/supportathon/supportathon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.supportathon.supportathon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:supportathon/main_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Support-a-thon',
+      home: MainView(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Start-o-thon"),
+      ),
+      body: const Center(child: Text("Testing")),
+    );
+  }
+}
